88821a2cba44597f42d95c67f55419a7991100bc
Juan José Gutiérrez Leguizamón <68406983+JuanGut06@users.noreply.github.com>
2024-05-28 15:11:18 -0500

    Initial commit

---
+++ AppToTripV3/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppToTripV3
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppToTripV3/Views/Splash.xaml.cs
@@ -0,0 +1,9 @@
+namespace AppToTripV3.Views;
+
+public partial class Splash : ContentView
+{
+	public Splash()
+	{
+		InitializeComponent();
+	}
+}
